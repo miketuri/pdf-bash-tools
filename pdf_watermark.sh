3a78bdd5aac9bf893cb27ba928fa6e849a8f9204
miketuri <miketuri6@gmail.com>
2023-05-08 17:41:36 -0700

    Added support to pdfmark.sh and pdf_watermark.sh to specify page numbers where watermark begins/ends and x/y-position of watermark

--- pdf_watermark.sh
+++ pdf_watermark.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -e
-# /opt/scripts/pdf_watermark.sh
-# @author: Nestor Urquiza
-# @date: 20180323
+# pdf_watermark.sh
+# @author: Mike Turi modifications of Nestor Urquiza's pdf_watermark.sh in pdf-bash-tools repository
+# @date: 20230508
 # @description: Creates a temporary pdfmark and merges it with the input pdf to produce a watermark with a custom text
-#
+#               It is optional to specify the x- and y-position for the watermark, where to start the watermark (first page number), where to end the watermark (last page number), and password.
 
-USAGE="Usage: `basename $0` <pdf_from_file> <pdf_to_file> <watermark> [password]"
+USAGE="Usage: `basename $0` <pdf_from_file> <pdf_to_file> <watermark> [xpos] [ypos] [first_page] [last_page] [password]"
  
 if [ $# -lt "3" ] 
 then
@@ -16,14 +16,29 @@ fi
 pdf_from_file=$1
 pdf_to_file=$2
 watermark=$3
-password=$4
+xpos=$4
+ypos=$5
+first_page=$6
+last_page=$7
+password=$8
+
+if [ $# -lt "4" ]
+then
+    xpos=130
+fi
+
+if [ $# -lt "5" ]
+then
+    ypos=70
+fi
 
 #tmpfile=$(mktemp /tmp/pdf_watermark.XXXXXX)
 tmpfile=mark.ps
 
-font="/Helvetica-Bold 72 selectfont"
-color=".75 setgray"
-angle=45
+#font="/Helvetica-Bold 72 selectfont"
+color="0.4 setgray"
+#color=".2 .2 .2 setrgbcolor"
+angle=50
 
 
 ! read -d '' pdf_mark <<EOF
@@ -33,11 +48,16 @@ angle=45
      2 eq { pop false }
      {
          gsave      
-         /Helvetica findfont 48 scalefont setfont
+         /Times-Roman findfont
+         48 scalefont
+         setfont
          newpath
-         .90 setgray 130 70 moveto 50 rotate
-         (${watermark}) false  charpath
-         1 setlinewidth stroke
+         $((xpos)) $(($ypos)) moveto
+         $(($angle)) rotate
+         (${watermark}) true charpath
+         0.25 setlinewidth
+         (${color})
+         stroke
          grestore
          true
      } ifelse
@@ -46,5 +66,5 @@ angle=45
 EOF
 
 echo  "$pdf_mark" > "$tmpfile" 
-./pdfmark.sh "$pdf_from_file" "$pdf_to_file" "$tmpfile" "$password"
+./pdfmark.sh "$pdf_from_file" "$pdf_to_file" "$tmpfile" "$first_page" "$last_page" "$password"
 rm "$tmpfile"
--- pdfmark.sh
+++ pdfmark.sh
@@ -1,11 +1,12 @@
 #!/bin/bash -e
-# /opt/scripts/pdfmark.sh
-# @author: Nestor Urquiza
-# @date: 20140501
+# pdfmark.sh
+# @author: Mike Turi's modifications of Nestor Urquiza's pdfmark.sh in pdf-bash-tools repository
+# @date: 20230508
 # @description: Accepts a pdfmark file. See http://partners.adobe.com/public/developer/en/acrobat/sdk/pdf/pdf_creation_apis_and_specs/pdfmarkReference.pdf
 #               Then it applies the metadata to the input pdf file generating the resulting output file
+#               It is optional to specify the where to start modifications (first page number), where to end modifications (last page number), and password.
 
-USAGE="Usage: `basename $0` <pdf_from_file> <pdf_to_file> <pdfmark_file> [password]"
+USAGE="Usage: `basename $0` <pdf_from_file> <pdf_to_file> <pdfmark_file> [first_page] [last_page] [password]"
  
 if [ $# -lt "3" ] 
 then
@@ -16,7 +17,23 @@ fi
 pdf_from_file=$1
 pdf_to_file=$2
 pdfmark_file=$3
-password=$4
+first_page=$4
+last_page=$5
+password=$6
+
+if [ -z $first_page ]
+then
+    firstpage_switch=""
+else
+    firstpage_switch="-dFirstPage=$first_page"
+fi
+
+if [ -z $last_page ]
+then
+    lastpage_switch=""
+else
+    lastpage_switch="-dLastPage=$last_page"
+fi
 
 if [ -z $password ]
 then
@@ -29,5 +46,5 @@ else
     pdf_pwd_switch="-sPDFPassword=$password"
 fi
 
-gs $pdf_pwd_switch $owner_pwd_switch $user_pwd_switch -dBATCH -dNOPAUSE -q -sOutputFile="$pdf_to_file" \
+gs $pdf_pwd_switch $owner_pwd_switch $user_pwd_switch -dBATCH -dNOPAUSE -q $firstpage_switch $lastpage_switch -dAutoRotatePages=/None -sOutputFile="$pdf_to_file" \
     -sDEVICE=pdfwrite "$pdfmark_file" "$pdf_from_file"
